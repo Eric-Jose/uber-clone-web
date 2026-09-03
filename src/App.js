83a8dc653bbddfdcde573ed19aadce6a9c51e413
Eric-Jose <101611891+Eric-Jose@users.noreply.github.com>
2026-09-03 08:29:26 -0400

    Exibir estatisticas ao vivo no painel do passageiro e motorista

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import NotificationCenter from './pages/NotificationCenter';
 import MapRideFixed from './pages/MapRideFixed';
 import RideHistory from './pages/RideHistory';
 import ResetPassword from './pages/ResetPassword';
+import LiveStatsBar from './pages/LiveStatsBar';
 import './App.css';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
@@ -166,11 +167,11 @@ function App() {
     case 'login': return <Login onLoginSuccess={handleUserLogin} />;
     case 'register': return <Register onRegisterSuccess={handleUserLogin} />;
     case 'reset-password': return <ResetPassword onBackToLogin={() => { window.history.replaceState({}, '', window.location.pathname); setCurrentPage('login'); }} />;
-    case 'ride': return <MapRideFixed onRideCreate={() => {}} onBack={() => setCurrentPage('profile')} />;
+    case 'ride': return user ? <><LiveStatsBar userType="passenger" /><MapRideFixed onRideCreate={() => {}} onBack={() => setCurrentPage('profile')} /></> : <Login onLoginSuccess={handleUserLogin} />;
     case 'ride-history': return user ? <RideHistory user={user} onBack={() => setCurrentPage(user.userType === 'driver' ? 'driver-dashboard' : 'ride')} /> : <Login onLoginSuccess={handleUserLogin} />;
     case 'driver-registration': return <DriverRegistration onRegistrationSubmit={handleDriverRegistrationSubmit} />;
     case 'driver-pending': return <DriverPending user={user} onLogout={handleUserLogout} />;
-    case 'driver-dashboard': return <DriverDashboard />;
+    case 'driver-dashboard': return user ? <><LiveStatsBar userType="driver" /><DriverDashboard /></> : <Login onLoginSuccess={handleUserLogin} />;
     case 'profile': return user ? <UserProfile user={user} onLogout={handleUserLogout} onRequestRide={() => setCurrentPage('ride')} onHistory={() => setCurrentPage('ride-history')} /> : <Login onLoginSuccess={handleUserLogin} />;
     case 'admin-panel': return <AdminPanel />;
     case 'payment': return <Payment rideId="RIDE001" amount={32.50} onPaymentSuccess={() => alert('Pagamento realizado!')} />;
